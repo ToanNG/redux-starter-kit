c0002148fedcd0cfbc5254ac7d276d4ce1871302
Toan Nguyen Gia <toan.nguyen@2359media.com>
2016-01-25 00:50:47 +0700

    install react, babel and try server-side rendering

---
+++ public/javascripts/components/List.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+  render = () => {
+    return (
+      <h1>List component</h1>
+    )
+  };
+}
+
+export default List
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express')
 var router = express.Router()
 
+import React from 'react'
+import ReactDOM from 'react-dom/server'
+import List from '../public/javascripts/components/List'
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.render('index', {
+    title: 'Express',
+    html: ReactDOM.renderToString(<List />)
+  })
 })
 
 module.exports = router
