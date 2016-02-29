73c1e2d538736b559df9f9d301cf050507aa604d
Toan Nguyen Gia <toan.nguyen@2359media.com>
2016-02-29 12:22:22 +0700

    install material-ui

--- shared/containers/App.js
+++ shared/containers/App.js
@@ -1,6 +1,15 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import RouteCSSTransitionGroup from './RouteCSSTransitionGroup'
+
+import RaisedButton from 'material-ui/lib/raised-button'
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin()
 
 class App extends Component {
   static propTypes = {
@@ -36,6 +45,7 @@ class App extends Component {
           {' '}
           <Link to='/about'>About</Link>
         </header>
+        <RaisedButton label='Primary' primary />
         <RouteCSSTransitionGroup
           component='div' transitionName='page'
           transitionEnterTimeout={500} transitionLeaveTimeout={250}
