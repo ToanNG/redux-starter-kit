a31a00b3a480187edbbaa8a888a17bb9f4f8e69f
Toan Nguyen Gia <toan.nguyen@2359media.com>
2016-04-24 08:53:06 +0700

    add task clean

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var runSequence = require('run-sequence')
 require('require-dir')('gulp-tasks')
 
 gulp.task('dev', function () {
-  return runSequence('webpack', 'service-worker', 'watch', 'nodemon')
+  return runSequence('clean', 'webpack', 'service-worker', 'watch', 'nodemon')
 })
 
 gulp.task('default', function (cb) {
-  return runSequence('webpack', 'service-worker')
+  return runSequence('clean', 'webpack', 'service-worker')
 })
